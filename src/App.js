e99be6f206ce1d3917084128daffda2ba1d87038
Usuario <Email Usuario>
2022-08-19 09:23:09 -0500

    CSS

--- src/App.js
+++ src/App.js
@@ -1,24 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import Planta from './Tipos/Planta.png';
+import Fuego from './Tipos/Fuego.png';
+import Tierra from './Tipos/Tierra.png';
+import Agua from './Tipos/Agua.png'
+
+import './css/bootstrap.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <ul class="nav">
+    <li class="nav-item">
+      <a class="nav-link active" href="#">Active</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="#">Link</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="#">Link</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link disabled" href="#">Disabled</a>
+    </li>
+  </ul>
+    <table class="table table-bordered">
+      <thead>
+        <th>Tipo</th>
+        <th>Fuerte contra</th>
+        <th>Debil contra</th>
+      </thead>
+      <tbody>
+        <tr>
+          <td scope="row">
+            <button class="btn btn-light"><img  src={Planta} alt="Tipo Planta" width="70px" /></button>
+            <button class="btn btn-light"><img src={Fuego} alt="Tipo Planta" width="70px" /></button>
+            <button class="btn btn-light"><img src={Tierra}  alt="Tipo Planta" width="70px" /></button>
+            <button class="btn btn-light"><img src={Agua} alt="Tipo Planta" width="70px" /></button>
+          </td>
+        </tr>
+      </tbody>
+      </table>
+      </>
   );
 }
 
